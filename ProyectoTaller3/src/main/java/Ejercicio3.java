ea62271bb8ac3c8f7bebc2558e1ccee9b206da3a
javiersolan010 <javisolanossr2@gmail.com>
2024-10-24 12:45:37 -0500

    proyectotaller3

--- ProyectoTaller3/src/main/java/Ejercicio3.java
+++ ProyectoTaller3/src/main/java/Ejercicio3.java
@@ -1,7 +1,7 @@
 public class Ejercicio3 {
     public static void main(String[] args) {
         double respuesta;
-        respuesta = Math.pow(10,2) + Math.sqrt(16) + ((20)/1+11);
+        respuesta = Math.pow(10,2) + Math.sqrt(16) + ((20)/12);
         System.out.println("respuesta = " + respuesta);
     }
 }
