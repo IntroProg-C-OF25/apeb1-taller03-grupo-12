3020a43d0a049c583f2a241fe44ac997c6e0ec0d
javiersolan010 <javisolanossr2@gmail.com>
2024-10-24 12:33:00 -0500

    proyectotaller3

--- ProyectoTaller3/src/main/java/Ejercicio2.java
+++ ProyectoTaller3/src/main/java/Ejercicio2.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Hans
- */
 public class Ejercicio2 {
-    
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta = Math.pow(10,2)+10;
+                System.out.println("respuesta = " + respuesta);
+    }
 }
