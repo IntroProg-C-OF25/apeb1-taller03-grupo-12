70e87cf3c6730bbfa509cc3beb82cf61bdddb9fc
javiersolan010 <javisolanossr2@gmail.com>
2024-10-27 00:07:55 -0500

    proyectotaller3

--- ProyectoTaller3/src/main/java/Ejercicio10.java
+++ ProyectoTaller3/src/main/java/Ejercicio10.java
@@ -1,12 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Hans
- */
 public class Ejercicio10 {
+    public static void main(String[] args) {
+        boolean a = Math.sqrt(25) * 10 >= 100;
+        boolean b = true;
+        boolean c = false;
+        boolean d = (10/5) >= 2;
+        System.out.println(a && b || c || d );
+    }
     
 }
