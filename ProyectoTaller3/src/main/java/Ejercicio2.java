2158800972dafdaa1b5cc2d01653d73d23468a00
hansalexander <hans06martinez@gmail.com>
2024-10-26 21:21:27 -0500

    proyectotaller3

--- ProyectoTaller3/src/main/java/Ejercicio2.java
+++ ProyectoTaller3/src/main/java/Ejercicio2.java
@@ -1,12 +1,8 @@
 public class Ejercicio2 {
-<<<<<<< HEAD
-    
-    
-=======
     public static void main(String[] args) {
         double respuesta;
         respuesta = Math.pow(10,2)+10;
                 System.out.println("respuesta = " + respuesta);
     }
->>>>>>> ea62271bb8ac3c8f7bebc2558e1ccee9b206da3a
+
 }
