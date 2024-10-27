832547b108b1fb33afd1c74bd6734063b08e037e
javiersolan010 <javisolanossr2@gmail.com>
2024-10-26 22:38:45 -0500

    proyectotaller3

--- ProyectoTaller3/src/main/java/Ejercicio4.java
+++ ProyectoTaller3/src/main/java/Ejercicio4.java
@@ -1,7 +1,7 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
         double respuesta;
-        respuesta = (20/(1+3)) + 2;
+        respuesta = (20/(1+2)) + 2;
         boolean res = respuesta >= 3;
         System.out.println("res = " + res);
     }
--- ProyectoTaller3/src/main/java/Ejercicio5.java
+++ ProyectoTaller3/src/main/java/Ejercicio5.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Hans
- */
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        System.out.println((2^2 + 12 - 1)/3);
+    }
     
 }
